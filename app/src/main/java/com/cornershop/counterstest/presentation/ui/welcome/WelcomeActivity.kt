2775b86fa43f40508a8d4561f691d66f6014de5a
caikovsky <caiqueanm@gmail.com>
2021-08-27 19:38:13 -0300

    welcome activity implementation

--- app/src/main/java/com/cornershop/counterstest/presentation/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/com/cornershop/counterstest/presentation/ui/welcome/WelcomeActivity.kt
@@ -1,12 +1,32 @@
 package com.cornershop.counterstest.presentation.ui.welcome
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.cornershop.counterstest.R
+import com.cornershop.counterstest.databinding.ActivityWelcomeBinding
+import com.cornershop.counterstest.presentation.ui.list.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class WelcomeActivity : AppCompatActivity() {
+
+    private var _binding: ActivityWelcomeBinding? = null
+    private val binding: ActivityWelcomeBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        _binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.welcomeContent?.buttonStart?.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    MainActivity::class.java
+                ).apply {
+                    flags =
+                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                })
+        }
     }
 }
