9b763ed4a2b29af19f88626e53c805eeca6efbf0
caikovsky <caiqueanm@gmail.com>
2021-08-27 20:12:51 -0300

    fix create counter viewmodel hilt injection error

--- app/src/main/java/com/cornershop/counterstest/presentation/ui/create/CreateCounterViewModel.kt
+++ app/src/main/java/com/cornershop/counterstest/presentation/ui/create/CreateCounterViewModel.kt
@@ -1,7 +1,11 @@
 package com.cornershop.counterstest.presentation.ui.create
 
+import androidx.lifecycle.ViewModel
+import com.cornershop.counterstest.domain.usecases.create.CreateCounterUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class CreateCounterViewModel {
+class CreateCounterViewModel @Inject constructor(createCounterUseCase: CreateCounterUseCase) :
+    ViewModel() {
 }
