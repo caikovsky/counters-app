18fc12133bd0e4a6d8972a934e7c12c0b4167665
Caique Antonelli Maurano <mauranocaique@gmail.com>
2021-08-31 21:33:09 -0300

    initial example fragment code

--- app/src/main/java/com/cornershop/counterstest/presentation/ui/examples/ExampleCounterFragment.kt
+++ app/src/main/java/com/cornershop/counterstest/presentation/ui/examples/ExampleCounterFragment.kt
@@ -1,8 +1,26 @@
 package com.cornershop.counterstest.presentation.ui.examples
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.cornershop.counterstest.databinding.FragmentExampleCounterBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ExampleCounterFragment: Fragment() {
+class ExampleCounterFragment : Fragment() {
+
+    private var _binding: FragmentExampleCounterBinding? = null
+    private val binding: FragmentExampleCounterBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentExampleCounterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
 }
