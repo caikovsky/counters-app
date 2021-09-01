612e2134d0396c6b9c523090b5f6b3fc2e24760e
Caique Antonelli Maurano <mauranocaique@gmail.com>
2021-08-31 22:07:31 -0300

    create chips programatically

--- app/src/main/java/com/cornershop/counterstest/presentation/ui/examples/ExampleCounterFragment.kt
+++ app/src/main/java/com/cornershop/counterstest/presentation/ui/examples/ExampleCounterFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.cornershop.counterstest.R
 import com.cornershop.counterstest.databinding.FragmentExampleCounterBinding
+import com.google.android.material.chip.Chip
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,6 +23,47 @@ class ExampleCounterFragment : Fragment() {
     ): View {
         _binding = FragmentExampleCounterBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        populateChips()
+    }
+
+    private fun populateChips() {
+        renderDrinkExamples()
+        renderFoodExamples()
+        renderMiscExamples()
+    }
+
+    private fun renderMiscExamples() {
+        val miscList = resources.getStringArray(R.array.misc_array)
+
+        for (misc in miscList) {
+            val chip = Chip(binding.miscExamplesChipGroup.context)
+            chip.text = misc
+            binding.miscExamplesChipGroup.addView(chip)
+        }
+    }
+
+    private fun renderFoodExamples() {
+        val foodList = resources.getStringArray(R.array.food_array)
+
+        for (food in foodList) {
+            val chip = Chip(binding.foodExamplesChipGroup.context)
+            chip.text = food
+            binding.foodExamplesChipGroup.addView(chip)
+        }
+    }
+
+    private fun renderDrinkExamples() {
+        val drinkList = resources.getStringArray(R.array.drinks_array)
+
+        for (drink in drinkList) {
+            val chip = Chip(binding.drinkExamplesChipGroup.context)
+            chip.text = drink
+            binding.drinkExamplesChipGroup.addView(chip)
+        }
     }
 
 }
