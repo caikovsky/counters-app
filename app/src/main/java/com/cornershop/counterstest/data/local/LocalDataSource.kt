64969133663a238077db3ae22095b45ce1603d69
Caique Antonelli Maurano <mauranocaique@gmail.com>
2021-09-02 01:17:34 -0300

    refactor local data source to use mapper

--- app/src/main/java/com/cornershop/counterstest/data/local/LocalDataSource.kt
+++ app/src/main/java/com/cornershop/counterstest/data/local/LocalDataSource.kt
@@ -1,11 +1,14 @@
 package com.cornershop.counterstest.data.local
 
+import com.cornershop.counterstest.data.mapper.CounterEntityMapper
 import com.cornershop.counterstest.domain.model.Counter
 import javax.inject.Inject
 
-class LocalDataSource @Inject constructor(private val dao: CounterDao) {
+class LocalDataSource @Inject constructor(private val dao: CounterDao, private val mapper: CounterEntityMapper) {
 
-    fun getCounters(): List<Counter> = dao.getCounters()
+    suspend fun getCounters(): List<Counter> = mapper.mapFromEntityList(dao.getCounters())
 
-    suspend fun createCounters(counters: List<Counter>) = dao.insertAll(counters)
+    suspend fun createCounters(counters: List<Counter>) = dao.insertAll(mapper.mapToEntityList(counters))
+
+    suspend fun createCounter(counter: Counter) = dao.insert(mapper.mapToEntity(counter))
 }
