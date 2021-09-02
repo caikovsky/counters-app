c485f39a21da15e2b6971064ea85fc4235a07f75
Caique Antonelli Maurano <mauranocaique@gmail.com>
2021-09-02 01:16:52 -0300

    remove unnecessary imports

--- app/src/main/java/com/cornershop/counterstest/domain/usecases/get/GetCounterUseCase.kt
+++ app/src/main/java/com/cornershop/counterstest/domain/usecases/get/GetCounterUseCase.kt
@@ -2,7 +2,6 @@ package com.cornershop.counterstest.domain.usecases.get
 
 import com.cornershop.counterstest.data.core.NetworkResult
 import com.cornershop.counterstest.domain.model.Counter
-import retrofit2.Response
 
 interface GetCounterUseCase {
 
