ed01901498d02cec84062e6ad070f9367245cd03
Caique Antonelli Maurano <mauranocaique@gmail.com>
2021-09-04 16:28:37 -0300

    set support action bar in main activity

--- app/src/main/java/com/cornershop/counterstest/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/cornershop/counterstest/presentation/ui/main/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
+        setSupportActionBar(binding.toolbar)
         val view = binding.root
         setContentView(view)
     }
