d8f59c72b52512f842ece31f3a53fa48352b7784
Caique Antonelli Maurano <mauranocaique@gmail.com>
2021-09-03 21:51:28 -0300

    one time splash screen with motion layout

--- app/src/main/java/com/cornershop/counterstest/presentation/ui/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/cornershop/counterstest/presentation/ui/splash/SplashScreenActivity.kt
@@ -1,36 +1,50 @@
-package com.cornershop.counterstest.presentation.ui.welcome
+package com.cornershop.counterstest.presentation.ui.splash
 
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.cornershop.counterstest.databinding.ActivityWelcomeBinding
+import androidx.lifecycle.lifecycleScope
+import com.cornershop.counterstest.R
 import com.cornershop.counterstest.presentation.ui.main.MainActivity
+import com.cornershop.counterstest.presentation.ui.welcome.WelcomeActivity
 import com.cornershop.counterstest.util.Constants.FIRST_ACCESS_SHARED_KEY
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class WelcomeActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
 
-    private var _binding: ActivityWelcomeBinding? = null
-    private val binding: ActivityWelcomeBinding get() = _binding!!
     private val sharedPreferencesName = "${packageName}_preferences"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        _binding = ActivityWelcomeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_splash)
 
         val sharedPref = getSharedPreferences(sharedPreferencesName, Context.MODE_PRIVATE)
         val firstAccess = sharedPref.getBoolean(FIRST_ACCESS_SHARED_KEY, true)
 
-        binding.welcomeContent?.buttonStart?.setOnClickListener {
-            if (firstAccess) {
-                sharedPref.edit().putBoolean(FIRST_ACCESS_SHARED_KEY, false).apply()
-            }
+        lifecycleScope.launch {
+            delay(1500)
 
-            goToMainActivity()
+            if (firstAccess) {
+                goToWelcomeActivity()
+            } else {
+                goToMainActivity()
+            }
         }
+    }
+
+    private fun goToWelcomeActivity() {
+        startActivity(
+            Intent(
+                this,
+                WelcomeActivity::class.java
+            ).apply {
+                flags =
+                    Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            })
     }
 
     private fun goToMainActivity() {
