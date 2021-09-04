dda6500d88a17ed8e94b9d48d10a87ae50dedae1
Caique Antonelli Maurano <mauranocaique@gmail.com>
2021-09-03 21:53:50 -0300

    local ip address

--- app/src/main/java/com/cornershop/counterstest/util/Constants.kt
+++ app/src/main/java/com/cornershop/counterstest/util/Constants.kt
@@ -1,7 +1,8 @@
 package com.cornershop.counterstest.util
 
 object Constants {
-    const val BASE_URL = "http://192.168.15.141:3000/api/v1/"
+    // TODO: change me!
+    const val BASE_URL = "http://192.168.15.8:3000/api/v1/"
     const val COUNTER_KEY = "counter"
     const val FIRST_ACCESS_SHARED_KEY = "first_access"
 }
