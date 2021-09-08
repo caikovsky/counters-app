43e4806f08a1fab18a690f85c407a208ea9cb289
Caique Antonelli Maurano <mauranocaique@gmail.com>
2021-09-07 22:48:08 -0300

    add mockk & a repository successfull test

---
+++ app/src/test/java/com/cornershop/counterstest/domain/usecases/get/GetCounterUseCaseImplTest.kt
@@ -0,0 +1,42 @@
+package com.cornershop.counterstest.domain.usecases.get
+
+import com.cornershop.counterstest.MainCoroutineRule
+import com.cornershop.counterstest.data.core.NetworkResult
+import com.cornershop.counterstest.data.repository.CounterRepository
+import com.cornershop.counterstest.domain.model.Counter
+import io.mockk.coEvery
+import io.mockk.mockk
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import okhttp3.ResponseBody
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import retrofit2.Response
+
+@ExperimentalCoroutinesApi
+class GetCounterUseCaseImplTest {
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    private val repository = mockk<CounterRepository>()
+    private lateinit var getCounterUseCase: GetCounterUseCase
+
+    @Before
+    fun setUp() {
+        getCounterUseCase = GetCounterUseCaseImpl(repository)
+    }
+
+    @Test
+    fun `should call repository and return a list of counter successfully`() = mainCoroutineRule.runBlockingTest {
+        val repositoryResponse = Response.success(200, listOf(Counter("id")))
+
+        coEvery { repository.getCounters() } returns repositoryResponse
+
+        val result = getCounterUseCase.invoke()
+
+        assertEquals(repositoryResponse.body(), result.data)
+    }
+}
