4605023a10d6f2b363ff6f1f2ea64179d07322fa
Caique Maurano <cmaurano@league.com>
2022-06-26 01:38:18 -0300

    chore: add create counter button

--- app/src/main/java/com/cornershop/counterstest/presentation/ui/list/ListCounterScreen.kt
+++ app/src/main/java/com/cornershop/counterstest/presentation/ui/list/ListCounterScreen.kt
@@ -3,10 +3,7 @@ package com.cornershop.counterstest.presentation.ui.list
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -14,6 +11,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -21,8 +19,8 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.cornershop.counterstest.R
 import com.cornershop.counterstest.presentation.model.Counter
+import com.cornershop.counterstest.presentation.ui.main.MainActivity.Routes
 import com.cornershop.counterstest.presentation.ui.theme.CounterTheme
-import com.cornershop.counterstest.presentation.ui.widgets.CounterTopAppBar
 import com.cornershop.counterstest.presentation.ui.widgets.LoadingScreen
 
 @Composable
@@ -36,11 +34,42 @@ internal fun ListCounterScreen(
     Scaffold(
         backgroundColor = CounterTheme.colors.background,
         topBar = {
-            CounterTopAppBar(
-                navController = navController,
-                titleResId = R.string.app_name
+            TopAppBar(
+                backgroundColor = CounterTheme.colors.secondary,
+                contentColor = CounterTheme.colors.primary,
+                title = { Text(text = stringResource(id = R.string.app_name)) },
+                elevation = 4.dp
             )
-        }
+        },
+        floatingActionButtonPosition = FabPosition.Center,
+        floatingActionButton = {
+            FloatingActionButton(
+                backgroundColor = CounterTheme.colors.primary,
+                onClick = { navController.navigate(Routes.CreateCounters.route) }
+            ) {
+                Row(
+                    modifier = modifier.padding(
+                        vertical = 20.dp,
+                        horizontal = 20.dp
+                    ),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_plus),
+                        contentDescription = null,
+                        tint = CounterTheme.colors.secondary
+                    )
+
+                    Spacer(modifier.width(8.dp))
+
+                    Text(
+                        text = stringResource(id = R.string.create_counter),
+                        color = CounterTheme.colors.secondary
+                    )
+                }
+            }
+        },
     ) { paddingValues ->
         Column(
             modifier = modifier
@@ -76,7 +105,7 @@ private fun CounterList(
 ) {
     LazyColumn(
         modifier = modifier.fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+        verticalArrangement = Arrangement.spacedBy(20.dp)
     ) {
         items(counters) { counter ->
             CounterItem(
